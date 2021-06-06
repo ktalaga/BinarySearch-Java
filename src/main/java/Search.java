7aa74eaaecea4b127f1eee3eee3316414e4fd1c5
konrad <ktalaga90@gmail.com>
2021-06-06 11:17:17 +0100

    linear search algorithm created, 2 tests passed

--- src/main/java/Search.java
+++ src/main/java/Search.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Search {
+
+    public boolean linearSearch(ArrayList<Integer> searchedArray, int searchedNumber){
+        for(int number : searchedArray){
+            if(number==searchedNumber){
+                return true;
+            }
+        }return false;
+    }
+
 }
